6a4285a7f92dba13a5c0a36db735150e7f2b8f48
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-07-31 17:39:37 +0200

    simplified toggle task done function

--- src/App.js
+++ src/App.js
@@ -23,9 +23,7 @@ function App() {
   const toggleTaskDone = (id) => {
     setTasks((tasks) =>
       tasks.map((task) => {
-        if (task.id === id) {
-          return { ...task, done: !task.done };
-        } else return task;
+        return task.id === id ? { ...task, done: !task.done } : task;
       })
     );
   };
