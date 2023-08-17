2af3a0e0d37590bc90cc78281df3ebc80ad2e8eb
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-08-17 17:13:25 +0200

    added react router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
-import Tasks from "./features/Tasks/Tasks";
+import Tasks from "./features/Tasks/TasksListPage";
+import Author from "./features/Author";
+import { Route, Link, HashRouter, Switch, Redirect } from "react-router-dom";
 
-export default () => <Tasks />;
+const App = () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/tasks"> Tasks </Link>
+        </li>
+        <li>
+          <Link to="/author">Author</Link>
+        </li>
+      </ul>
+    </nav>
+    <Switch>
+      <Route path="/tasks">
+        <Tasks />
+      </Route>
+      <Route path="/author">
+        <Author />
+      </Route>
+      <Route path="/">
+        <Redirect to="/tasks" />
+      </Route>
+    </Switch>
+  </HashRouter>
+);
+
+export default App;
