dbc097d4cf1e186dcd116659e4fac3defb687739
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-08-19 20:52:09 +0200

    fixed path to file

--- src/features/Tasks/getExampleTasks.js
+++ src/features/Tasks/getExampleTasks.js
@@ -1,5 +1,5 @@
 export const getExampleTasks = async () => {
-  const response = await fetch("/to-do-list-react/public/exampleTasks.json");
+  const response = await fetch("/To-do-list-in-React/exampleTasks.json");
 
   if (!response.ok) {
     new Error(response.statusText);
