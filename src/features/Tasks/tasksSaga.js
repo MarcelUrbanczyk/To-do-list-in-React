03ab4804de6623eb317b8537f98c139a330bbe5b
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-08-19 20:53:21 +0200

    created tasksSaga

---
+++ src/features/Tasks/tasksSaga.js
@@ -0,0 +1,23 @@
+import { takeLatest, put, call, takeEvery, select } from "redux-saga/effects";
+import { fetchExampleTasks, selectTasks, setTasks } from "./tasksSlice";
+import { getExampleTasks } from "./getExampleTasks";
+import { saveAtLocalStorage } from "./tasksLocalStorage";
+
+function* fetchExampleTasksHandler() {
+  try {
+    const exampleTasks = yield call(getExampleTasks);
+    yield put(setTasks(exampleTasks));
+  } catch (error) {
+    yield call(alert, "Something went wrong!");
+  }
+}
+
+function* saveTasksInLocalStorageHandler() {
+  const tasks = yield select(selectTasks);
+  yield call(saveAtLocalStorage, tasks);
+}
+
+export function* tasksSaga() {
+  yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+  yield takeEvery("*", saveTasksInLocalStorageHandler);
+}
