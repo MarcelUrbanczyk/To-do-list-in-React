49f0cb4dc5236f9625ea02d5ddcc6de7ae4d908c
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-08-20 22:38:28 +0200

    added Portrait to author page

---
+++ src/features/Author/Portrait/index.js
@@ -0,0 +1,13 @@
+import { Image, Frame, Wrapper } from "./styled";
+
+const Portrait = () => {
+  return (
+    <Wrapper>
+      <Frame>
+        <Image src="/To-do-list-in-React/prof.PNG" alt="Authors photo" />
+      </Frame>
+    </Wrapper>
+  );
+};
+
+export default Portrait;
--- src/features/Author/index.js
+++ src/features/Author/index.js
@@ -1,9 +1,11 @@
 import Section from "../../common/Section";
 import Header from "../../common/Header";
+import Portrait from "./Portrait";
 
 const Author = () => {
   return (
     <main>
+      <Portrait />
       <Header title="Author" />
       <Section title="Marcel Urbańczyk"></Section>
     </main>
