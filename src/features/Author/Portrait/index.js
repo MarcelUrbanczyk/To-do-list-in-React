1e4bdf2ad56b773f7317f45a50963a7bb8adf1ee
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-08-24 23:38:34 +0200

    deleted unnecessary code

--- src/features/Author/Portrait/index.js
+++ src/features/Author/Portrait/index.js
@@ -1,13 +1,11 @@
 import { Image, Frame, Wrapper } from "./styled";
 
-const Portrait = () => {
-  return (
-    <Wrapper>
-      <Frame>
-        <Image src="/To-do-list-in-React/prof.PNG" alt="Authors photo" />
-      </Frame>
-    </Wrapper>
-  );
-};
+const Portrait = () => (
+  <Wrapper>
+    <Frame>
+      <Image src="/To-do-list-in-React/prof.PNG" alt="Authors photo" />
+    </Frame>
+  </Wrapper>
+);
 
 export default Portrait;
--- src/features/Navigation/index.js
+++ src/features/Navigation/index.js
@@ -1,23 +1,21 @@
 import { StyledLink, Item, List } from "./styled";
 
-const Navigation = () => {
-  return (
-    <nav>
-      <List>
-        <Item>
-          <StyledLink activeClassName="active" to="/tasks">
-            {" "}
-            Tasks{" "}
-          </StyledLink>
-        </Item>
-        <Item>
-          <StyledLink activeClassName="active" to="/author">
-            Author
-          </StyledLink>
-        </Item>
-      </List>
-    </nav>
-  );
-};
+const Navigation = () => (
+  <nav>
+    <List>
+      <Item>
+        <StyledLink activeClassName="active" to="/tasks">
+          {" "}
+          Tasks{" "}
+        </StyledLink>
+      </Item>
+      <Item>
+        <StyledLink activeClassName="active" to="/author">
+          Author
+        </StyledLink>
+      </Item>
+    </List>
+  </nav>
+);
 
 export default Navigation;
